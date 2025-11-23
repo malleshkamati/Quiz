a50297c1f334b4ca0ce7fab4495bc42fa040b6ea
Mallesh Kamaati <143433175+malleshkamati@users.noreply.github.com>
2025-11-23 16:59:49 +0530

    Clean up App.jsx by removing unused code
    Removed commented-out code and unnecessary imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,4 @@
-// import { useState,useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import Quizform from './components/Quizform'
 
-
-
-
-
-// function App(props) {
-//   const [que_num,set_que_num]=useState(0)
-//   const [quizForms, setQuizForms] = useState([<Quizform key={0} addQuizForm={addQuizForm} />]);
-
-//   function addQuizForm() {
-//     setQuizForms(prevQuizForms => [
-//       ...prevQuizForms,
-//       <Quizform key={prevQuizForms.length} addQuizForm={addQuizForm} />
-//     ]); 
-//     set_que_num(prevQueNum => prevQueNum + 1);
-//   }
-//     // useEffect(addQuizForm,[que_num])
-  
-  
-//   return (
-//     <>
-//     <div className="bg-gray-100 flex justify-center items-center min-h-screen ">
-//      {/* <Quizform que_num={que_num} set_que_num={set_que_num} addQuizForm={addQuizForm}/>  */}
-// {/*     
-//      <div>{addQuizForm}</div> */}
-//      <div>
-//      {quizForms.map((key) => (
-//         <Quizform key={key} que_num={key + 1} set_que_num={set_que_num} addQuizForm={addQuizForm} />
-//       ))}
-//       </div>
-//      </div>
-     
-//     </>
-//   )
-// }
-
-// export default App
 
 
 import { useState } from 'react';
